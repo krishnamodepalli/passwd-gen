1166c1820df8cc5a06234e568dc9e4ef389b0b9a
Jail-Breaker <mailaboutlogin@gmail.com>
2021-12-08 07:23:25 +0530

    A simple Passwd generator made with random2 module.
    This is a very small python program to create random and strong passwd's,
    
    that are hard to determine and crack.
    
    We include lower case letters, UPPER case letter, numbers and $ymbo|s.

---
+++ main.py
@@ -0,0 +1,17 @@
+import random2 as rn
+
+letters = 'abcdefghijklmnopqrstuvwxyz'
+nums = '0123456789'
+sym = '`~!@#$%^&*()_+[]{-=}\|;":,./<>?'
+
+total = letters+letters.upper()+nums+sym
+
+
+def generator(length):
+    passwd = ''
+    for i in range(0, length):
+        passwd += rn.choice(total)
+    return passwd
+
+
+print(generator(16))
